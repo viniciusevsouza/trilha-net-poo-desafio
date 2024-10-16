f74a7472e6f03d3d40a7a45a520e6e169b310031
Vinicius Evangelista <vinicius.ev.souza@gmail.com>
2024-10-16 14:43:17 -0300

    Desafio concluido[C

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("912345678", "Nokia Teste", "123456789123456", 64);
+
+Console.WriteLine("Smartphone Nokia");
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Nokia teste");
+
+Iphone i1 = new Iphone("922345678", "Iphone Teste", "223456789123456", 128);
+
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone iPhone");
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Iphone teste");
